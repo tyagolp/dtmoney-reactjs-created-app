a3e1ecb00b5e420abbb64cce63ffb76774a54811
tiago <tyagolp@gmail.com>
2022-04-08 00:03:16 -0300

    add react modal and component modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,36 @@ import { Dashboard } from "./componets/Dashboard";
 import { Header } from "./componets/Header";
 import { GlobalStyle } from "./styles/global";
 
+import Modal from 'react-modal';
+import { useState } from "react";
+import { NewTransactionModal } from "./componets/NewTransactionModal";
+
+Modal.setAppElement('#root')
 
 export function App() {
+
+
+  const [isNewTransactionModalpen, serIsNewTransactionModalpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    serIsNewTransactionModalpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    serIsNewTransactionModalpen(false)
+  }
+
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
--- src/componets/Dashboard/index.tsx
+++ src/componets/Dashboard/index.tsx
@@ -1,19 +1,13 @@
-import logoImg from '../../assets/logo.svg'
 import { Summary } from '../Summary'
+import { TransactionTable } from '../TransactionTable'
 
-import { Container, Content } from './styles'
+import { Container } from './styles'
 
 export function Dashboard() {
     return (
         <Container>
             <Summary />
-            <Content>
-                <img src={logoImg} alt="dt money" />
-                <button type="button">
-                    Nova Transação
-                </button>
-
-            </Content>
+            <TransactionTable />
         </Container>
     )
 
